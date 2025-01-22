9aca30e64d6a62d426319ece5f3b476a705b22a1
JeremiasPiontkoski <jeremiaspion@gmail.com>
2025-01-22 20:49:22 -0300

    Add response class

--- source/Controllers/UserController.php
+++ source/Controllers/UserController.php
@@ -23,6 +23,7 @@ class UserController extends Controller
         if ($validator->fails()) {
             echo json_encode($validator->getErrors());
         }
+        
 
         return null;
     }
