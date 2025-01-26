84f806681a2f6821e1940ecd01492acc367f8305
JeremiasPiontkoski <jeremiaspion@gmail.com>
2025-01-26 14:03:20 -0300

    Add model Category

---
+++ source/Models/Category.php
@@ -0,0 +1,12 @@
+<?php
+namespace Source\Models;
+
+use CoffeeCode\DataLayer\DataLayer;
+
+class Category extends DataLayer
+{
+    public function __construct()
+    {
+        parent::__construct("categories", ["user_id, name"]);
+    }
+}
