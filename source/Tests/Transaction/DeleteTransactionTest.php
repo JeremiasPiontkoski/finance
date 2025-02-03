8fa91f808e0d5ae3ae5a6bc56b34f908277e48dc
JeremiasPiontkoski <jeremiaspion@gmail.com>
2025-02-02 23:44:47 -0300

    feat: add delete transaction tests

---
+++ source/Tests/Transaction/DeleteTransactionTest.php
@@ -0,0 +1,67 @@
+<?php
+namespace Source\Tests\Transaction;
+
+use Source\Controllers\TransactionController;
+use Source\Models\Test;
+
+class DeleteTransactionTest extends Test
+{
+    public function testSuccess(): void
+    {
+        $transactionController = new TransactionController();
+
+        ob_start();
+        $transactionController->delete(['id' => 1]);
+        $response = json_decode(ob_get_clean(), true);
+
+        $this->assertEquals("success", $response['status']);
+        $this->assertEquals(200, $response['statusCode']);
+        $this->assertArrayHasKey("message", $response);
+        $this->assertArrayHasKey("data", $response);
+    }
+
+    public function testIdAsString(): void
+    {
+        $transactionController = new TransactionController();
+
+        ob_start();
+        $transactionController->delete(['id' => 'a']);
+        $response = json_decode(ob_get_clean(), true);
+
+        $this->assertEquals("error", $response['status']);
+        $this->assertEquals(400, $response['statusCode']);
+        $this->assertArrayHasKey("message", $response);
+        $this->assertArrayHasKey("data", $response);
+        $this->assertArrayHasKey("id", $response['data']);
+    }
+
+    public function testInvalidId(): void
+    {
+        $transactionController = new TransactionController();
+
+        ob_start();
+        $transactionController->delete(['id' => 2]);
+        $response = json_decode(ob_get_clean(), true);
+
+        $this->assertEquals("error", $response['status']);
+        $this->assertEquals(400, $response['statusCode']);
+        $this->assertArrayHasKey("message", $response);
+        $this->assertArrayHasKey("data", $response);
+        $this->assertArrayHasKey("id", $response['data']);
+    }
+
+    public function testPermissionDenied(): void
+    {
+        $transactionController = new TransactionController();
+
+        ob_start();
+        $transactionController->delete(['id' => 6]);
+        $response = json_decode(ob_get_clean(), true);
+
+        $this->assertEquals("error", $response['status']);
+        $this->assertEquals(403, $response['statusCode']);
+        $this->assertArrayHasKey("message", $response);
+        $this->assertArrayHasKey("data", $response);
+        $this->assertArrayHasKey("user", $response['data']);
+    }
+}
